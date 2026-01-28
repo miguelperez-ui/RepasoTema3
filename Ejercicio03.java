a6348ad053363c2990916336db90bac05e0a4a87
miguelperez-ui <miguel.perez@iesnervion.es>
2026-01-28 08:57:40 +0100

    Update Ejercicio03.java

--- Ejercicio03.java
+++ Ejercicio03.java
@@ -3,8 +3,55 @@ package repaso;
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String texto = "Hola soy Miguel y estoy haciendo un ejercicio de repaso";
+
+		String claves[] = { "caballo", "tortiLLa", "Miguel", "caramelo", "esToy" };
+		
+		String[]encontradas=palabrasEncontradas(texto,claves);
+		mostrarResultado(encontradas);
+
+	}
+
+	static boolean contienePalabra(String texto, String palabra) {
+		texto.toLowerCase();
+		palabra.toLowerCase();
+		return texto.contains(palabra);
+	}
+
+	static String[] palabrasEncontradas(String texto, String[] claves) {
+		int contador = 0;
+		for (int i = 0; i < claves.length; i++) {
+			if (contienePalabra(texto, claves[i])) {
+				contador++;
+			}
+		}
+		String resultado[] = new String[contador];
+
+		int pos = 0;
+
+		for (int i = 0; i < claves.length; i++) {
+			if (contienePalabra(texto, claves[i])) {
+				resultado[pos] = claves[i];
+				pos++;
+
+			}
+		}
+		return resultado;
+	}
+
+	static void mostrarResultado(String[] encontradas) {
+		if (encontradas.length == 0) {
+			System.out.println("No se han encontrado palabras clave en el texto");
+		} else {
+			System.out.println("Palabras encontradas: ");
+			for (int i = 0; i < encontradas.length; i++) {
+				System.out.print(String.join(", ", encontradas));
+				if (i < encontradas.length - 1) {
+					System.out.print(", ");
+				}
+			}
+		}
 	}
 
 }
